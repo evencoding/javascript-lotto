cd6ba901dc4abe7e67c6ac0699ef5c236722c796
evencoding <evencoding@gmail.com>
2022-12-06 14:46:38 +0900

    feat: 중복되지 않는 임의의 6개의 숫자를 뽑아 오름차순으로 정렬하는 기능 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,9 +1,17 @@
+const LottoGame = require('../service/LottoGame');
+
 const Validator = require('../Validator');
 
 const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
 class LottoController {
+  #lottoGame;
+
+  constructor() {
+    this.#lottoGame = new LottoGame();
+  }
+
   gameStart() {
     this.#inputBudget();
   }
@@ -18,6 +26,12 @@ class LottoController {
       OutputView.printErrorMessage(message);
       throw new Error('[ERROR]');
     }
+
+    this.#buyLottos(budget / 1000);
+  }
+
+  #buyLottos(count) {
+    const lottos = this.#lottoGame.generateLottos(count);
   }
 }
 
--- src/service/LottoGame.js
+++ src/service/LottoGame.js
@@ -1,3 +1,13 @@
-class LottoGame {}
+const { Random } = require('@woowacourse/mission-utils');
+
+class LottoGame {
+  #lottos;
+
+  generateLottos(count) {
+    return Array.from({ length: count }).map(() =>
+      Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
+    );
+  }
+}
 
 module.exports = LottoGame;
