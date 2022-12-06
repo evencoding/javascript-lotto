113e86e62bc977c4b518edbe7a444a4cf6e83556
evencoding <evencoding@gmail.com>
2022-12-06 14:25:39 +0900

    feat: 로또 구입 금액을 입력하는 기능 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,5 +1,15 @@
+const InputView = require('../views/InputView');
+
 class LottoController {
-  gameStart() {}
+  gameStart() {
+    this.#inputBudget();
+  }
+
+  #inputBudget() {
+    InputView.askBudget(this.#validateBudget.bind(this));
+  }
+
+  #validateBudget(budget) {}
 }
 
 module.exports = LottoController;
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,9 @@
-const InputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = {
+  askBudget(callback) {
+    Console.readLine('구입금액을 입력해주세요.\n', callback);
+  },
+};
 
 module.exports = InputView;
